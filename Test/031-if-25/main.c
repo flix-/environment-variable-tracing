25874a1376f06718ddc87a232c694a2e4a7d400a
Sebastian Roland <seroland86@gmail.com>
2019-02-08 12:59:28 +0100

    add

---
+++ Test/031-if-25/main.c
@@ -0,0 +1,30 @@
+extern char *getenv(const char *);
+
+int
+main()
+{
+    int a,b,c,d,e;
+
+    if (a) {
+        do {
+            if ((getenv("gude") && a && b && c) || e) {
+                if (a) {
+                    do {
+                        int a = 0;
+                    } while (0);
+                } else {
+                    int b = 1;
+                }
+            } else if (a || b || c) {
+                int d = a;
+            }
+            int tainted = 1;
+        } while (getenv("gude"));
+        int end = 1;
+    }
+
+    int ut = 0;
+
+    return 0;
+}
+
