30b5157b8d21848ee312855dedbce20398763c65
Sebastian Roland <seroland86@gmail.com>
2019-02-14 21:41:39 +0100

    push

--- Tools/analysis-driver.pl
+++ Tools/analysis-driver.pl
@@ -74,34 +74,34 @@ while (my $function = <$funs_fh>) {
 close($funs_fh);
 close($ir_fh);
 
-print "Merging trace files\n";
-
-my @traces;
-
-foreach my $trace_file (glob("static-*-trace.txt")) {
-    print "Procesing $trace_file: ";
-    if (-z $trace_file) {
-        print "Skipping\n";
-        next;
-    }
-    print "Adding\n";
-
-    push @traces, $trace_file;
-}
-
-print "No valid trace files found\n" and exit if (!@traces);
-
-my $lcov_param = "";
-
-foreach my $trace_file (@traces) {
-    $lcov_param .=  "-a $trace_file ";
-}
-
-my $merged_trace_file = "static-trace.txt";
-
-system("lcov $lcov_param -o $merged_trace_file");
-
-print "Creating HTML page\n";
-
-system("genhtml -o html $merged_trace_file");
+#print "Merging trace files\n";
+#
+#my @traces;
+#
+#foreach my $trace_file (glob("static-*-trace.txt")) {
+#    print "Procesing $trace_file: ";
+#    if (-z $trace_file) {
+#        print "Skipping\n";
+#        next;
+#    }
+#    print "Adding\n";
+#
+#    push @traces, $trace_file;
+#}
+#
+#print "No valid trace files found\n" and exit if (!@traces);
+#
+#my $lcov_param = "";
+#
+#foreach my $trace_file (@traces) {
+#    $lcov_param .=  "-a $trace_file ";
+#}
+#
+#my $merged_trace_file = "static-trace.txt";
+#
+#system("lcov $lcov_param -o $merged_trace_file");
+#
+#print "Creating HTML page\n";
+#
+#system("genhtml -o html $merged_trace_file");
 
